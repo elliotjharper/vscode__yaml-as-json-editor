b24c90dd34d44e008cabc3a2b1d619c6be8ee961
elliotjharper <50680194+elliotjharper@users.noreply.github.com>
2024-05-05 13:06:22 +0100

    Little bit of tidy up

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { YamlAndJsonEditorHost } from './yaml-and-json.editor-provider';
+import { YamlAndJsonEditorHost } from './yaml-and-json.editor-host';
 
 export function activate(context: vscode.ExtensionContext) {
     // Register our custom editor provider
